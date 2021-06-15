57b7c64d47e1be74107026b2102447c5f6440a1c
JP Eiler <jp.eiler97@gmail.com>
2021-06-15 10:04:18 -0500

    added imports and test code to src files

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Foo from './components/Foo.js';
+
+function App() {
+	return <Foo />;
+}
+
+export default App;
---
+++ src/components/Foo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Foo() {
+	return (
+		<div className="main-container">
+			<div className="container">
+				<h1>Foo!</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Foo;
