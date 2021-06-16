20dc487a34fb9b96cd65d947bb3e2bcc1de433fa
JP Eiler <jp.eiler97@gmail.com>
2021-06-16 16:03:27 -0500

    users can be viewed in asc/desc order by id

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,37 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Sort extends Component {
-	// Setting the initial state of the Counter component
-	state = {
-		sortOrder: -1,
-		sortText: 'Descending'
-	};
-
-	// handleIncrement increments this.state.count by 1
-	setDescending = () => {
-		// We always use the setState method to update a component's state
-		this.setState({ sortOrder: -1, sortText: 'Descending' });
-	};
-	setAscending = () => {
-		// We always use the setState method to update a component's state
-		this.setState({ sortOrder: 1, sortText: 'Ascending' });
-	};
-	// The render method returns the JSX that should be rendered
-	render() {
-		return (
-			<div className="card-body">
-				<p className="card-text">
-					Order: {this.state.sortText} ({this.state.sortOrder})
-				</p>
-				<button className="btn btn-primary" onClick={this.setDescending}>
-					Descend
-				</button>
-				<button className="btn btn-primary" onClick={this.setAscending}>
-					Ascend
-				</button>
-			</div>
-		);
-	}
+function Sort({ order, category, setAscending, setDescending, handleOrderSubmit }) {
+	return (
+		<div className="card-body">
+			<p className="card-text">
+				Order: {order} {category}
+			</p>
+			<button className="btn btn-primary" onClick={setDescending}>
+				Descend
+			</button>
+			<button className="btn btn-primary" onClick={setAscending}>
+				Ascend
+			</button>
+			<button className="btn btn-primary" onClick={handleOrderSubmit}>
+				Submit
+			</button>
+		</div>
+	);
 }
 
 export default Sort;
--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -4,9 +4,23 @@ import UserInfo from './UserInfo';
 import Sort from './Sort';
 
 class UserTable extends Component {
-	handleOrderChoice = (event) => {
+	state = {
+		order: 'descending',
+		category: 'id',
+		userList: users[0].results
+	};
+
+	setAscending = () => {
+		this.setState({ order: 'ascending' });
+	};
+
+	setDescending = () => {
+		this.setState({ order: 'descending' });
+	};
+
+	handleOrderSubmit = (event) => {
 		event.preventDefault();
-		this.searchGiphy(this.state.search);
+		this.searchGiphy(this.state.order);
 	};
 
 	render() {
@@ -21,18 +35,39 @@ class UserTable extends Component {
 							<th scope="col">Phone</th>
 						</tr>
 					</thead>
-					{users[0].results.map((user) => (
-						<UserInfo
-							key={user.id}
-							id={user.id}
-							firstName={user.name.first}
-							lastName={user.name.last}
-							email={user.email}
-							phone={user.phone}
-						/>
-					))}
+					{this.state.order === 'ascending' ? (
+						[ ...this.state.userList ]
+							.reverse()
+							.map((user) => (
+								<UserInfo
+									key={user.id}
+									id={user.id}
+									firstName={user.name.first}
+									lastName={user.name.last}
+									email={user.email}
+									phone={user.phone}
+								/>
+							))
+					) : (
+						[ ...this.state.userList ].map((user) => (
+							<UserInfo
+								key={user.id}
+								id={user.id}
+								firstName={user.name.first}
+								lastName={user.name.last}
+								email={user.email}
+								phone={user.phone}
+							/>
+						))
+					)}
 				</table>
-				<Sort />
+				<Sort
+					order={this.state.order}
+					category={this.state.category}
+					setAscending={this.setAscending}
+					setDescending={this.setDescending}
+					handleOrderSubmit={this.handleOrderSubmit}
+				/>
 			</div>
 		);
 	}
